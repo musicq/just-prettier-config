e96ee2094c0faa66535d763e6897044d2323bb8e
musicq <liang.kui@hotmail.com>
2021-01-04 23:56:37 +0800

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  printWidth: 80,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  bracketSpacing: false,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+}
